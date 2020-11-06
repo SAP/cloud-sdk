38c1a95ada3e2802955665d95b2263e3efbbb64c
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-11-06 13:54:02 +0100

    Fix links

--- src/sap/sdk-js/BuildBadge/index.js
+++ src/sap/sdk-js/BuildBadge/index.js
@@ -4,7 +4,7 @@ function BuildBadge ({ align }) {
   return (
     <span className={align ? 'col text--right' : ''}>
       <a target='_blank' rel='noopener noreferrer' href='https://github.com/SAP/cloud-sdk'>
-        <img src='https://github.com/SAP/cloud-sdk/workflows/build/badge.svg' alt='build' className='max-width:100%;' />
+        <img src='https://github.com/SAP/cloud-sdk-js/workflows/build/badge.svg' alt='build' className='max-width:100%;' />
       </a>
     </span>
   )
