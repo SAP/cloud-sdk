e5cec0c8273eb1bee66d362faa105f3dd806d35d
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-02-24 20:07:16 +0100

    Add custom fields

--- docs/js/features/odata/common/entity/index.jsx
+++ docs/js/features/odata/common/entity/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import CustomFieldsContent from './custom-fields.mdx';
 import DeserializeEntityContent from './deserialize-entity.mdx';
 import EntityBuilderContent from './entity-builder.mdx';
 import EntityContent from './entity.mdx';
 import FromJsonContent from './from-json.mdx';
+
+export function CustomFields() {
+  return <CustomFieldsContent />;
+}
 
 export function DeserializeEntity() {
   return <DeserializeEntityContent />;
