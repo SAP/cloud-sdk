ad399b3e18698ec41f0ce8bda3963dc67fc13771
Florian Richter <florian.richter@sap.com>
2021-03-02 13:31:18 +0100

    Fix typos

--- src/sap/sdk-data/data.js
+++ src/sap/sdk-data/data.js
@@ -10,10 +10,10 @@ export const features = [
       'Fluent API for type-safe OData calls',
       'Both OData v2 and OData v4 covered',
       'Discover OData APIs from your IDE',
-      `Clean code - low to no boilerplate`,
-      `Kepp focus on a business logic`
+      'Clean code - low to no boilerplate',
+      'Keep focus on a business logic'
     ],
-    img: `img/feat/feat-type-safe-client.svg`
+    img: 'img/feat/feat-type-safe-client.svg'
   },
   {
     id: 'openapi',
@@ -23,9 +23,9 @@ export const features = [
       'Fluent API for type-safe OpenAPI calls',
       'OpenAPI  2.0 and 3.0',
       'Support for vendor extensions',
-      `Deep integration with SDK's features`
+      "Deep integration with SDK's features"
     ],
-    img: `img/feat/feat-openapi2.svg`
+    img: 'img/feat/feat-openapi2.svg'
   },
   {
     id: 'btp',
@@ -35,10 +35,10 @@ export const features = [
       'Embedded multi-tenancy',
       'Support all authentication flows',
       'Multiple runtimes covered',
-      'Cloud to On-Premise connetivity',
-      'Advance Cloud quality patterns'
+      'Cloud to On-Premise connectivity',
+      'Advance cloud quality patterns'
     ],
-    img: `img/feat/feat-btp-cloud.svg`
+    img: 'img/feat/feat-btp-cloud.svg'
   },
   {
     id: 'clients',
@@ -46,46 +46,51 @@ export const features = [
     benefits: [
       'SAP S/4HANA Cloud, SAP S/4HANA On-Premise, Workflow and more...',
       <>
-        Find then on{' '}
+        Available on{' '}
         <a href="https://search.maven.org/search?q=g:com.sap.cloud.sdk*">
           Maven Central
         </a>
       </>,
       <>
-        Also on npm{' '}
-        <a href="https://www.npmjs.com/search?q=sap%2Fsap-cloud-vdm*">here</a>{' '}
+        Available on npm for{' '}
+        <a href="https://www.npmjs.com/search?q=sap%2Fsap-cloud-vdm*">
+          SAP S/4HANA Cloud
+        </a>
         and{' '}
-        <a href="https://www.npmjs.com/search?q=sap%2Fsap-cloud-op*">here</a>
+        <a href="https://www.npmjs.com/search?q=sap%2Fsap-cloud-op*">
+          SAP S/4HANA On-Premise
+        </a>
       </>
     ],
-    img: `img/feat/feat-code.svg`
+    img: 'img/feat/feat-code.svg'
   },
   {
     id: 'bas',
-    title: 'Enjoy Coding with Business Application Studio - BAS',
+    title: 'Enjoy Coding with Business Application Studio',
     benefits: [
       <>
         Both Java and Typescript/JavaScript SDKs support{' '}
         <a href="https://developers.sap.com/tutorials/appstudio-onboarding.html">
           Business Application Studio
-        </a>
+        </a>{' '}
+        (BAS)
       </>,
       'Code you application without leaving your browser',
       'Connect your repository directly to BAS'
     ],
-    img: `img/feat/feat-bas.svg`
+    img: 'img/feat/feat-bas.svg'
   },
   {
     id: 'support',
     title: 'First Class Support',
     benefits: [
       'We take support seriously',
-      'By Developers for Developers',
+      'By developers - For Developers',
       'Quick and proactive response',
       <>
-        <a href={ghIssuesLink}>Public GitHub Issue</a> for convenience
+        <a href={ghIssuesLink}>Public GitHub issues</a> for convenience
       </>
     ],
-    img: `img/feat/feat-support.svg`
+    img: 'img/feat/feat-support.svg'
   }
 ];
