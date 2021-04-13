35e4b441bc7ff9ba04ba01ef9c8e775c038a77a7
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2021-04-13 13:22:31 +0200

    Custom Axios options (request config) (#213)

---
+++ docs/js/features/common/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CustomRequestConfigContent from './custom-request-config.mdx';
+import CustomRequestConfigNoteContent from './custom-request-config-note.mdx';
+
+export function CustomRequestConfig() {
+  return <CustomRequestConfigContent />;
+}
+
+export function CustomRequestConfigNote() {
+  return <CustomRequestConfigNoteContent />;
+}
