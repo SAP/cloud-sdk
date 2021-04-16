f6b9f49b283ff48a813089d773c5f0d817effd0e
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2021-04-16 07:49:59 +0200

    csrf token updates (#224)

--- docs/js/features/odata/common/index.jsx
+++ docs/js/features/odata/common/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import CsrfContent from './csrf-token.mdx';
+import SkipCsrfContent from './skip-csrf-token.mdx';
+import SkipCsrfExampleContent from './skip-csrf-token-example.mdx';
 import EtagContent from './etag.mdx';
 
 export function Csrf() {
   return <CsrfContent />;
+}
+
+export function SkipCsrf() {
+  return <SkipCsrfContent />;
+}
+
+export function SkipCsrfExample() {
+  return <SkipCsrfExampleContent />;
 }
 
 export function Etag() {
