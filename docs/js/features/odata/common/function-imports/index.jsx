8ba349b114c40e32411958ac6050ee7083edf796
Junjie Tang <junjie.tang@sap.com>
2021-04-25 11:15:25 +0200

    entity return type

--- docs/js/features/odata/common/function-imports/index.jsx
+++ docs/js/features/odata/common/function-imports/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RequestBuilderContent from './request-builder.mdx';
 import EtagFunctionImportContent from './etag-function-import.mdx';
+import EntityReturnTypeContent from './entity-return-type.mdx';
 
 export function RequestBuilder() {
   return <RequestBuilderContent />;
@@ -8,4 +9,8 @@ export function RequestBuilder() {
 
 export function EtagFunctionImport() {
   return <EtagFunctionImportContent />;
+}
+
+export function EntityReturnType() {
+  return <EntityReturnTypeContent />;
 }
