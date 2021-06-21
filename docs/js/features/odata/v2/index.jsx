6bdd2950ebe9ef1540649772e141830a47bff675
Kavitha <kavitha.sivakumar@sap.com>
2021-06-21 13:05:34 +0530

    Add snippet to JS docs

--- docs/js/features/odata/v2/index.jsx
+++ docs/js/features/odata/v2/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import SelectContent from './select.mdx';
+import DocsRating from '../../../../../src/sap/common-utils/DocsRating';
 
 export function Select() {
   return <SelectContent />;
 }
+<DocsRating />
