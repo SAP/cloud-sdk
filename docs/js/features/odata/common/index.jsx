565ed04666486eb369fa0b01c64dc3df4a708e26
Johannes Schneider <johannes-schneider_1995@web.de>
2021-07-05 09:06:54 +0200

    Revert "Improve Feedback Snippet Visibility (#353)"
    This reverts commit 0aa17bfe3874fa5bf1ecba87e2598ce4b92461b0.

--- docs/js/features/odata/common/index.jsx
+++ docs/js/features/odata/common/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CsrfContent from './csrf-token.mdx';
 import SkipCsrfExampleContent from './skip-csrf-token-example.mdx';
 import EtagContent from './etag.mdx';
+import DocsRating from '../../../../../src/sap/common-utils/DocsRating';
 
 
 export function Csrf() {
@@ -15,3 +16,4 @@ export function SkipCsrfExample() {
 export function Etag() {
   return <EtagContent />;
 }
+<DocsRating />
