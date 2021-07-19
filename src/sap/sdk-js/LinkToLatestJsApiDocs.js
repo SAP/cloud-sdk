d13b955b2f95df53ec53530a818d485ce6404e9e
Artem Kovalov <artem.kovalyov@gmail.com>
2021-07-19 10:19:24 +0200

    Remove .html for GH pages

--- src/sap/sdk-js/LinkToLatestJsApiDocs.js
+++ src/sap/sdk-js/LinkToLatestJsApiDocs.js
@@ -2,9 +2,7 @@ import React from 'react';
 import JsSdkVersionList from '../../../static/api/versions';
 
 function LinkToLatestJsApiDocs({ slug, name }) {
-  return (
-    <a href={`/cloud-sdk/api/${JsSdkVersionList[0]}/${slug}.html`}>{name}</a>
-  );
+  return <a href={`/cloud-sdk/api/${JsSdkVersionList[0]}/${slug}`}>{name}</a>;
 }
 
 export default LinkToLatestJsApiDocs;
