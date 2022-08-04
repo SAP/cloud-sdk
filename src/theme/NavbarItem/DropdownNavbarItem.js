d44e66f300219dc3686771acfd80994ec2408eb2
I519840 <tom.frenken@sap.com>
2022-08-04 10:18:42 +0200

    minor api reference improvement

--- src/theme/NavbarItem/DropdownNavbarItem.js
+++ src/theme/NavbarItem/DropdownNavbarItem.js
@@ -3,20 +3,29 @@ import { shouldShow } from './helper';
 import DropdownNavbarItem from '@theme-original/NavbarItem/DropdownNavbarItem';
 import sdkVersions from '@site/static/api/versions';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import {
+  useActiveVersion
+} from '@docusaurus/plugin-content-docs/client';
 
 export default function DropdownNavbarItemWrapper(props) {
     if (shouldShow(props) && props.apiReference) {
+      const currentVersion = useActiveVersion(props.docsPluginId).label[1];
       const newProps = {
         ...props,
         items: sdkVersions.reduce((prevArray, version) => {
+          if(version[0] === currentVersion){
+            return [
+              ...prevArray,
+              {
+                label: version,
+                href: useBaseUrl(`api/${version}/`)
+              }
+            ]
+          }
           return [
-            ...prevArray,
-            {
-              label: version,
-              href: useBaseUrl(`api/${version}/`)
-            }
+            ...prevArray
           ]
-        }, []) // use reduce algorithm here to return an array of all versions with the form of {label: 2.6.0, href={useBaseUrl(api/${version}/`)}>{`${version}`}}
+        }, [])
       }
       return <DropdownNavbarItem {...newProps} />;
     }
