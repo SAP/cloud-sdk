459cd1b79e50ad301ccd92f635f4bf47448adf99
deekshas8 <deeksha.sinha@sap.com>
2022-09-30 14:23:27 +0200

    clean up docs

--- sidebarsDocsJs.js
+++ sidebarsDocsJs.js
@@ -98,6 +98,7 @@ module.exports = {
       items: ['video/demo-of-cloud-sdk-for-javascript-with-calm-and-cap']
     },
     'release-notes-sap-cloud-sdk-for-javascript-and-typescript',
+    'support-js',
     'frequently-asked-questions'
   ]
 };
