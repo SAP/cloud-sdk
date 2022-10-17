24b533cea07b414ff55b368f50c85c483b4a16b2
Johannes Schneider <johannes-schneider_1995@web.de>
2022-10-17 16:05:40 +0200

    add individual release note files

---
+++ docs-java/release-notes/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Notes400To415 from './release-notes-4.0.0-to-4.15.0.mdx';
+
+export function ReleaseNotes400To415() {
+    return <Notes400To415 />
+}
---
+++ docs-java_versioned_docs/version-v3/release-notes/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Notes300To315 from './release-notes-3.0.0-to-3.15.0.mdx';
+import Notes316To330 from './release-notes-3.16.0-to-3.30.0.mdx';
+import Notes331To345 from './release-notes-3.31.0-to-3.45.0.mdx';
+import Notes346To360 from './release-notes-3.46.0-to-3.60.0.mdx';
+import Notes361To375 from './release-notes-3.61.0-to-3.75.0.mdx';
+
+export function ReleaseNotes300To315() {
+    return <Notes300To315 />
+}
+
+export function ReleaseNotes316To330() {
+    return <Notes316To330 />
+}
+
+export function ReleaseNotes331To345() {
+    return <Notes331To345 />
+}
+
+export function ReleaseNotes346To360() {
+    return <Notes346To360 />
+}
+
+export function ReleaseNotes361To375() {
+    return <Notes361To375 />
+}
