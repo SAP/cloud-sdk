caadcb177bde35f361b3d9fe4c05c20b61d4577b
Johannes Schneider <johannes-schneider_1995@web.de>
2022-10-25 16:45:46 +0200

    style

--- docs-java_versioned_docs/version-v3/release-notes/index.jsx
+++ docs-java_versioned_docs/version-v3/release-notes/index.jsx
@@ -7,25 +7,25 @@ import Notes60To74 from './release-notes-60-to-74.mdx';
 import Notes75To89 from './release-notes-75-to-89.mdx';
 
 export function ReleaseNotes0To14() {
-    return <Notes0To14 />
+  return <Notes0To14 />;
 }
 
 export function ReleaseNotes15To29() {
-    return <Notes15To29 />
+  return <Notes15To29 />;
 }
 
 export function ReleaseNotes30To44() {
-    return <Notes30To44 />
+  return <Notes30To44 />;
 }
 
 export function ReleaseNotes45To59() {
-    return <Notes45To59 />
+  return <Notes45To59 />;
 }
 
 export function ReleaseNotes60To74() {
-    return <Notes60To74 />
+  return <Notes60To74 />;
 }
 
 export function ReleaseNotes75To89() {
-    return <Notes75To89 />
+  return <Notes75To89 />;
 }
