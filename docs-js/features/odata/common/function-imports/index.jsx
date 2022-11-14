284ee76be158e3a9d9473fa32fb0e24e13667ab1
Florian Wilhelm <florian.wilhelm02@sap.com>
2022-11-14 10:29:59 +0100

    Update OData operations docs to include bound ops (#1119)

--- docs-js/features/odata/common/function-imports/index.jsx
+++ docs-js/features/odata/common/function-imports/index.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import RequestBuilderContent from './request-builder.mdx';
 import EtagFunctionImportContent from './etag-function-import.mdx';
 import EntityReturnTypeContent from './entity-return-type.mdx';
-
-export function RequestBuilder() {
-  return <RequestBuilderContent />;
-}
 
 export function EtagFunctionImport() {
   return <EtagFunctionImportContent />;
