e9de7d982cc6a08157cddb41963cad95c5c4dbd1
Florian Wilhelm <florian.wilhelm02@sap.com>
2022-11-21 16:51:30 +0100

    Branch v3 docs

---
+++ docs-js_versioned_docs/version-v2/features/odata/common/function-imports/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import EtagFunctionImportContent from './etag-function-import.mdx';
+import EntityReturnTypeContent from './entity-return-type.mdx';
+
+export function EtagFunctionImport() {
+  return <EtagFunctionImportContent />;
+}
+
+export function EntityReturnType() {
+  return <EntityReturnTypeContent />;
+}
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ module.exports = {
         },
         {
           label: '🚀 JavaScript',
-          to: 'docs/js/overview',
+          to: 'docs/js/v2/overview',
           position: 'left',
           docsPluginId: 'docs-js',
           activeBasePath: 'docs/js',
@@ -134,7 +134,7 @@ module.exports = {
           items: [
             {
               label: 'Tutorials',
-              to: 'docs/js/tutorials/getting-started/introduction'
+              to: 'docs/js/v2/tutorials/getting-started/introduction'
             },
             {
               label: 'npm',
@@ -150,7 +150,7 @@ module.exports = {
             },
             {
               label: 'Support',
-              to: 'docs/js/support'
+              to: 'docs/js/v2/support'
             }
           ]
         },
@@ -259,10 +259,17 @@ module.exports = {
         editUrl: 'https://github.com/SAP/cloud-sdk/edit/main',
         routeBasePath: 'docs/js',
         sidebarPath: require.resolve('./sidebarsDocsJs.js'),
-        lastVersion: 'current',
+        lastVersion: 'v2',
         versions: {
           current: {
+            label: 'v3',
+            banner: 'unreleased',
+            badge: false
+          },
+          v2: {
             label: 'v2',
+            path: 'v2',
+            banner: 'none',
             badge: false
           },
           v1: {
@@ -283,7 +290,7 @@ module.exports = {
               '/docs/js/frequently-asked-questions',
               '/docs/js/guides/migrate-to-open-source-version-of-cloud-sdk-for-javascript-typescript'
             ],
-            to: '/docs/js/overview'
+            to: '/docs/js/v2/overview'
           },
           {
             from: ['/docs/community', '/docs/related-projects'],
@@ -291,103 +298,103 @@ module.exports = {
           },
           {
             from: '/docs/js/support-js',
-            to: '/docs/js/support'
+            to: '/docs/js/v2/support'
           },
           {
             from: '/docs/js/release-notes-sap-cloud-sdk-for-javascript-and-typescript',
-            to: '/docs/js/release-notes'
+            to: '/docs/js/v2/release-notes'
           },
           {
             from: '/docs/js/overview-cloud-sdk-for-javascript',
-            to: '/docs/js/overview'
+            to: '/docs/js/v2/overview'
           },
           {
             from: '/docs/js/video/demo-of-cloud-sdk-for-javascript-with-calm-and-cap',
-            to: '/docs/js/video/calm'
+            to: '/docs/js/v2/video/calm'
           },
           {
             from: '/docs/js/guides/migrate-sdk-application-from-btp-cf-to-kubernetes',
-            to: '/docs/js/guides/kubernetes'
+            to: '/docs/js/v2/guides/kubernetes'
           },
           {
             from: '/docs/js/guides/bas-external-system',
-            to: '/docs/js/guides/bas'
+            to: '/docs/js/v2/guides/bas'
           },
           {
             from: '/docs/js/guides/how-to-use-the-approuter',
-            to: '/docs/js/guides/approuter'
+            to: '/docs/js/v2/guides/approuter'
           },
           {
             from: '/docs/js/guides/how-to-add-resilience',
-            to: '/docs/js/guides/resilience'
+            to: '/docs/js/v2/guides/resilience'
           },
           {
             from: '/docs/js/guides/how-to-retrieve-jwt',
-            to: '/docs/js/guides/retrieve-jwt'
+            to: '/docs/js/v2/guides/retrieve-jwt'
           },
           {
             from: '/docs/js/features/connectivity/destination',
-            to: '/docs/js/features/connectivity/destinations'
+            to: '/docs/js/v2/features/connectivity/destinations'
           },
           {
             from: '/docs/js/features/odata/use-odata-v2-type-safe-client-for-javascript-typescript',
-            to: '/docs/js/features/odata/v2-client'
+            to: '/docs/js/v2/features/odata/v2-client'
           },
           {
             from: '/docs/js/features/odata/use-odata-v4-type-safe-client-for-javascript-typescript',
-            to: '/docs/js/features/odata/v4-client'
+            to: '/docs/js/v2/features/odata/v4-client'
           },
           {
             from: '/docs/js/features/connectivity/destination-cache-isolation',
-            to: '/docs/js/features/connectivity/destination-cache'
+            to: '/docs/js/v2/features/connectivity/destination-cache'
           },
           {
             from: '/docs/js/guides/sdk-in-browser',
-            to: '/docs/js/guides/browser'
+            to: '/docs/js/v2/guides/browser'
           },
           {
             from: '/docs/js/guides/trust-and-key-stores',
-            to: '/docs/js/guides/trust-and-keystores'
+            to: '/docs/js/v2/guides/trust-and-keystores'
           },
           {
             from: '/docs/js/features/connectivity/generic-http-client',
-            to: '/docs/js/features/connectivity/http-client'
+            to: '/docs/js/v2/features/connectivity/http-client'
           },
           {
             from: '/docs/js/guides/debug-remote-app',
-            to: '/docs/js/guides/remote-debugging'
+            to: '/docs/js/v2/guides/remote-debugging'
           },
           {
             from: '/docs/js/features/connectivity/proxy',
-            to: '/docs/js/features/connectivity/proxies'
+            to: '/docs/js/v2/features/connectivity/proxies'
           },
           {
             from: '/docs/js/tutorials/address-manager/read-addresses',
-            to: '/docs/js/tutorials/address-manager/read'
+            to: '/docs/js/v2/tutorials/address-manager/read'
           },
           {
             from: '/docs/js/tutorials/address-manager/change-addresses',
-            to: '/docs/js/tutorials/address-manager/change'
+            to: '/docs/js/v2/tutorials/address-manager/change'
           },
           {
             from: '/docs/js/features/odata/generate-odata-client',
-            to: '/docs/js/features/odata/generate-client'
+            to: '/docs/js/v2/features/odata/generate-client'
           },
           {
             from: '/docs/js/features/odata/execute-odata-request',
-            to: '/docs/js/features/odata/execute-request'
+            to: '/docs/js/v2/features/odata/execute-request'
           },
           {
             from: '/docs/js/features/openapi/execute-openapi-request',
-            to: '/docs/js/features/openapi/execute-request'
+            to: '/docs/js/v2/features/openapi/execute-request'
           },
           {
             from: '/docs/js/features/openapi/generate-openapi-client',
-            to: '/docs/js/features/openapi/generate-client'
+            to: '/docs/js/v2/features/openapi/generate-client'
           },
           {
             from: '/docs/js/troubleshooting-js',
-            to: '/docs/js/troubleshooting'
+            to: '/docs/js/v2/troubleshooting'
           },
           {
             from: '/docs/java/environments/sap-btp-environments-overview',
