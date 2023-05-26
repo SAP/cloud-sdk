2179bc40f0518095ce589bba3650b2fb1cde23e3
Christoph Schubert <christoph.schubert@sap.com>
2023-05-26 10:59:26 +0200

    [Java] Release Notes 4.15.0

--- docs-java/release-notes/index.jsx
+++ docs-java/release-notes/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Notes0To14 from './release-notes-0-to-14.mdx';
+import Notes15To29 from './release-notes-15-to-29.mdx';
 
 export function ReleaseNotes0To14() {
   return <Notes0To14 />;
+}
+
+export function ReleaseNotes15To29() {
+  return <Notes15To29 />;
 }
