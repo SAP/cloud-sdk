e0ce3ffeabaec233e40fd91c88873102a78b1524
Charles Dubois <103174266+CharlesDuboisSAP@users.noreply.github.com>
2025-03-26 13:13:21 +0100

    Revert "Algolia key in environment variable (#2056)" (#2060)
    This reverts commit 847b9e2a1a8b3deccdd4c18ebee0cfec504869cc.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,4 @@
 const { ProvidePlugin } = require('webpack');
-import 'dotenv/config';
 
 // We have to polyfill some Node APIs because Docusaurus migrated to Webpack 5
 // This is mainly required because of remark related modules which don't load otherwise
@@ -38,7 +37,7 @@ module.exports = {
       darkTheme: require('prism-react-renderer').themes.dracula
     },
     algolia: {
-      apiKey: process.env.ALGOLIA_API_KEY || 'fake_key',
+      apiKey: '441c57554e5a0ca9338cc9f047fb10c8',
       indexName: 'sap_cloud-sdk',
       appId: 'E4A268JVO0',
       algoliaOptions: {} // Optional, if provided by Algolia
