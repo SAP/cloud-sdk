fa33ebc372e02345d64b65a85badc26983048280
Zhongpin Wang <zhongpin.wang@sap.com>
2025-04-16 11:25:43 +0200

    update eslint relevant deps

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["build/**/*", "node_modules/**/*", "static/**/*"]), {
+    extends: compat.extends("plugin:react/recommended", "prettier"),
+
+    plugins: {
+        react,
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            Atomics: "readonly",
+            SharedArrayBuffer: "readonly",
+        },
+
+        ecmaVersion: 11,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        "react/prop-types": "off",
+    },
+}]);
