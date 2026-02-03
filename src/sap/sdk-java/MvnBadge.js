f7ba2b95875b0dc1d1112c90217074124869ae24
Alexander Dümont <alexander.duemont@sap.com>
2026-02-03 17:39:55 +0100

    Update Maven badge URLs in MvnBadge.js

--- src/sap/sdk-java/MvnBadge.js
+++ src/sap/sdk-java/MvnBadge.js
@@ -3,10 +3,10 @@ import React from 'react';
 function MvnBadge({ align }) {
   return (
     <span className={align ? 'col text--right' : ''}>
-      <a href="https://maven-badges.herokuapp.com/maven-central/com.sap.cloud.sdk/sdk-bom">
+      <a href="https://maven-badges.sml.io/maven-central/com.sap.cloud.sdk/sdk-bom">
         <img
           alt="maven central"
-          src="https://maven-badges.herokuapp.com/maven-central/com.sap.cloud.sdk/sdk-bom/badge.svg"
+          src="https://maven-badges.sml.io/maven-central/com.sap.cloud.sdk/sdk-bom/badge.svg"
         />
       </a>
     </span>
