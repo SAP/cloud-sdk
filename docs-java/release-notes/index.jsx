14f2703a3a98c8bd2aba0f885adc4dd8c373fa38
Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>
2026-05-06 16:44:24 +0200

    30 to 44

--- docs-java/release-notes/index.jsx
+++ docs-java/release-notes/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Notes0To14 from './release-notes-0-to-14.mdx';
 import Notes15To29 from './release-notes-15-to-29.mdx';
+import Notes30To44 from './release-notes-30-to-44.mdx';
 
 export function ReleaseNotes0To14() {
   return <Notes0To14 />;
@@ -8,4 +9,8 @@ export function ReleaseNotes0To14() {
 
 export function ReleaseNotes15To29() {
   return <Notes15To29 />;
+}
+
+export function ReleaseNotes30To44() {
+  return <Notes30To44 />;
 }
