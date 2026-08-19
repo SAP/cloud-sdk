902e592cc02b0339b87801d793102a2ce9d5f49c
I769216 <guilherme.pretto@sap.com>
2026-08-19 14:14:08 -0300

    update overview and add a new section called features to contain all SDK modules grouped by type

--- sidebarsDocsPython.js
+++ sidebarsDocsPython.js
@@ -2,6 +2,17 @@ module.exports = {
   docsPythonSidebar: [
     'overview',
     'getting-started',
+    {
+      type: 'category',
+      label: 'Features',
+      collapsed: false,
+      items: [
+        'features/agent-framework',
+        'features/connectivity',
+        'features/core-modules',
+        'features/btp-services',
+      ]
+    },
     'release-notes',
     'support',
     'troubleshooting'
